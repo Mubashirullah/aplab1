bc327bf781a6a4528b1e609746a26de698b46d53
mubashirullah <mdurrani.bscs16seecs@seecs.edu.pk>
2018-09-14 00:58:09 +0500

    Method Structure changed

--- MatrixLib/MatrixMultiplication.cpp
+++ MatrixLib/MatrixMultiplication.cpp
@@ -8,34 +8,29 @@ using namespace std;
 
 vector <vector <double>>Class1::Multiply(vector<vector<double>> A, vector<vector<double>> B)
 {
-	int columnA = A[0].size;
+	int columnA = A[0].size;									//Saving variables to check dimensions.
 	int rowB = B.size;
+
+	int rowA = A.size;
+	int colB = B[0].size;                                    //Dimensions for the new matrix. rowA * colB.
+	vector<vector<double>> C(rowA, vector<double>(colB));
 
 	if (columnA != rowB)
 	{
 		cout << "Dimension not compatible" << endl;
-		return;
 	}
 	else
-	{
-		int rowA = A.size;
-		int colB = B[0].size;                                    //Dimensions for the new matrix. rowA * colB.
-		vector<vector<double>> C( rowA, vector<double>(colB) ); //We know the size so declare it such. Again, arrays would have been perfect!
-										
-
+	{					
 		for (int i = 0; i < rowA; i++)
 		{
 			for (int k = 0; k < colB; k++)
 			{
-				
 				for (int inner = 0; inner < columnA; inner++)
 				{
-					
 					C[i][k] += A[i][inner] * B[inner][k];
 				}
 			}
 		}
-
-		return C;
 	}
+	return C;
 }
